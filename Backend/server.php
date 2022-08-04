2b6fe06827bd7b074af8fd5516816bf67d450613
XaraVasilakopoulou <charikleiaeirinivasilakopoulou@gmail.com>
2022-08-04 20:38:25 +0300

    API creation

--- Backend/server.php
+++
@@ -1,7 +0,0 @@
-<?php
-    header('Access-Control-Allow-Origin: http://localhost:3000');
-    $user = $_POST['email'];
-    $pass = $_POST['password'];
-    echo ("Hello from server:   $pass");
-    echo ("Hello from server: $user and ");
-?>
