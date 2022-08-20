b752c1d965edd2b6eeb425f70351658b8aefa0f5
XaraVasilakopoulou <charikleiaeirinivasilakopoulou@gmail.com>
2022-08-20 20:12:00 +0300

    Create Map

--- Frontend/src/User/Map.js
+++ Frontend/src/User/Map.js
@@ -1,21 +1,20 @@
 import React from 'react'
-import {MapContainer, TileLayer, Popup, Marker} from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import './Map.css'
 
 export const Map = ()=> {
-const position = [51.505, -0.09]
         
-return(
-
-  <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
-)
+return (
+<MapContainer center={[38.24, 21.73]} zoom={13} scrollWheelZoom={true}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[38.2466, 21.7345]}>
+    <Popup>
+      COVID IS THERE <br /> STAY SAFE BUDDY.
+    </Popup>
+  </Marker>
+</MapContainer>
+);
 }
