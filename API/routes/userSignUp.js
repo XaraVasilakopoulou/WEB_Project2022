7ef0cb0e7c314a9fd5ad2030a406650e9d0ffa42
MarinosGkizas <80125435+MarinosGkizas@users.noreply.github.com>
2022-08-23 21:35:13 +0300

    .

--- API/routes/userSignUp.js
+++ API/routes/userSignUp.js
@@ -19,8 +19,8 @@ connection.connect((err)=>{
 
 router.get('/', (req, res) => {
   connection.connect(function(err) {
-    let stmt = `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`;
-    let user = [req.data.username, req.data.email, req.data.password]
+    let stmt = `INSERT INTO users (username, email, password, speciality) VALUES (?, ?, ?,?)`;
+    let user = [req.data.username, req.data.email, req.data.password, 'user']
    
     connection.query(stmt, user, function (err, result, fields) {
       if (err) throw err;
